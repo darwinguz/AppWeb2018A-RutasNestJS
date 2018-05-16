0e7115b6f5603922fc2f631419ddfac10a133c5c
WRAD <dga_guz@hotmail.es>
2018-05-16 10:51:39 -0500

    Lectura de archivos en Inicio corregida

--- NestJSProjectRutas/src/inicio.controller.ts
+++ NestJSProjectRutas/src/inicio.controller.ts
@@ -10,6 +10,8 @@ export class InicioController {
         @Req() request,
         @Res() response
     ) {
+        let contenidoFinal = '';
+
         let footer = '';
         fs.readFile(
             __dirname + '/html/footer.html',
@@ -22,53 +24,53 @@ export class InicioController {
                         .send('Error');
                 } else {
                     footer = contenidoFooter;
-                    return response
-                        .status(200)
-                        .send(contenidoFooter);
+                    // return response
+                    //     .status(200)
+                    //     .send(contenidoFooter);
+
+                    let header = '';
+                    fs.readFile(
+                        __dirname + '/html/header.html',
+                        'utf8',
+                        (error, contenidoHeader) => {
+                            if (error) {
+                                console.log('Error', error);
+                                return response
+                                    .status(500)
+                                    .send('Error');
+                            } else {
+                                header = contenidoHeader;
+                                // return response
+                                //     .status(200)
+                                //     .send(contenidoHeader);
+                                let contenido = '';
+                                fs.readFile(
+                                    __dirname + '/html/contenido.html',
+                                    'utf8',
+                                    (error, contenidoContenido) => {
+                                        if (error) {
+                                            console.log('Error', error);
+                                            return response
+                                                .status(500)
+                                                .send('Error');
+                                        } else {
+                                            contenido = contenidoContenido;
+                                            contenidoFinal = header.concat(contenido).concat(footer);
+                                            console.log("1 IN: " + contenidoFinal);
+                                            return response
+                                                .status(200)
+                                                .send(contenidoFinal);
+                                        }
+                                    }
+                                );
+                            }
+                        }
+                    );
                 }
             }
         );
 
-        let header = '';
-        fs.readFile(
-            __dirname + '/html/header.html',
-            'utf8',
-            (error, contenidoHeader) => {
-                if (error) {
-                    console.log('Error', error);
-                    return response
-                        .status(500)
-                        .send('Error');
-                } else {
-                    header = contenidoHeader;
-                    return response
-                        .status(200)
-                        .send(contenidoHeader);
-                }
-            }
-        );
-
-        let contenido = '';
-        fs.readFile(
-            __dirname + '/html/contenido.html',
-            'utf8',
-            (error, contenidoContenido) => {
-                if (error) {
-                    console.log('Error', error);
-                    return response
-                        .status(500)
-                        .send('Error');
-                } else {
-                    contenido = contenidoContenido;
-                    return response
-                        .status(200)
-                        .send(contenidoContenido);
-                }
-            }
-        );
-
-        let contenidoFinal = header.concat(contenido).concat(footer);
-        console.log(contenidoFinal);
-        return contenidoFinal;
+        console.log("2 OUT: " + contenidoFinal);
+        return "Espere un momento";
     }
 }
